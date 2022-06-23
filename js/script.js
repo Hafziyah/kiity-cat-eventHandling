6aa03a5c26f1bfa9b223b10b1fed58be7643fa8a
Hafsat_A <68890886+Hafziyah@users.noreply.github.com>
2022-06-23 11:35:58 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let showCat = document.querySelector(".show-cat");
+let cat = document.querySelector(".cat");
+//console.log(showCat);
+
+showCat.addEventListener("click", function () {
+  cat.classList.add("show");
+});
